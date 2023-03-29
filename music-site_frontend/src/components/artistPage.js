205ad41dd430217464f7db503e3e96cccd276f37
kbugaite <kbugaite@gmail.com>
2023-03-29 12:15:10 +0100

    updated artistpage

--- music-site_frontend/src/components/artistPage.js
+++ music-site_frontend/src/components/artistPage.js
@@ -4,20 +4,38 @@ import LoadingSpinner from './spinner';
 import { useParams } from 'react-router-dom';
 
 function ArtistPage(props) {
-    const [artistInfoFilled, setartistInfoFilled] = useState(false);
+    const [artistInfoFilled, setartistInfoFilled] = useState(false)
     const [artistInfo, setArtistInfo] = useState({})
     const [artistSocials, setArtistSocials] = useState([])
     const [artistImages, setArtistImages] = useState([])
+    const [artistAlbum, setArtistAlbum] = useState([])
+    const [albumInfo, setAlbumInfo] = useState([])
     const [bio, setBio] = useState("")
 
     const { artistId } = useParams()
 
     const getArtistInfo = async (uri) => {
         const data = await props.client.getArtist(uri);
+        const albumMapped = data.artist.discography.albums.items
         setArtistInfo(data)
         setArtistSocials(data.artist.profile.externalLinks.items)
         setArtistImages(data.artist.visuals.gallery.items)
+        setArtistAlbum(data.artist.discography.albums.items)
+        getDiscography("5HOHne1wzItQlIYmLXLYfZ")
+        // getDiscography(data.artist.discography.albums.items[0].releases.items[0].id)
+        console.log(data.artist.discography.albums.items)
+
+        const mappedTrackResults= albumMapped.map(
+            albumTracks => 
+            getDiscography(albumTracks.releases.items[0].id)
+        )
         setBio(data.artist.profile.biography.text)
+    }
+
+    const getDiscography = async (uri) => {
+        const albumData = await props.client.getAlbum(uri);
+        setAlbumInfo(albumData.album.tracks.items);
+        console.log("getDiscography's URI is set to: " + uri)
     }
 
     useEffect(() => {
@@ -71,25 +89,43 @@ function ArtistPage(props) {
                 </div>
                 <br />
                 <div className="grid grid-cols-2 gap-2">
-                    <div>
-                        <h2 className="artistHeading py-4">Albums</h2>
+                    <div className="ml-6">
+                        <h2 className="artistHeading py-4">Latest release</h2>
                         {/* Latest album */}
                         <img className="rounded-lg" src={artistInfo?.artist?.discography?.latest?.coverArt?.sources[0]?.url} width={imageSize} />
                         <h3 className="albumHeading py-4">{artistInfo?.artist?.discography?.latest?.name} | {artistInfo?.artist?.discography?.latest?.date?.year}</h3>
                         {/* Table element - All albums */}
-                        <div className="discographyTable">
+                        <div className="artistTable">
                             <Table>
                                 <Table.Head>
                                     <Table.HeadCell>
-                                        Discography
+                                    {artistInfo?.artist?.profile?.name}'s Discography
+                                    </Table.HeadCell>
+                                    <Table.HeadCell>
+                                        Tracks
                                     </Table.HeadCell>
                                 </Table.Head>
                                 <Table.Body className="divide-y">
-                                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                            TO DO - MAP THROUGH ARTIST'S ALBUMS
-                                        </Table.Cell>
-                                    </Table.Row>
+                                        {artistAlbum.map(album => {
+                                            return (
+                                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                                    <img className="rounded-lg artistImage" src={album.releases?.items[0]?.coverArt?.sources?.[0]?.url} width={300}/>
+                                                    <h2 className="text-gray-900 font-bold py-1 mt-4 pb-4 text-center">{album.releases?.items[0]?.name}</h2>
+                                                    <h3 className="text-gray-800 py-1 text-center">Release year: {album.releases?.items[0]?.date?.year}</h3>
+                                                    <h3 className="text-gray-800 py-1 text-center">Number of tracks: {album.releases?.items[0]?.tracks?.totalCount}</h3>
+                                                    <h3 className="text-gray-800 py-1 text-center">Label: {album.releases?.items[0]?.label}</h3>
+                                                </Table.Cell>
+                                                <Table.Cell className="whitespace-wrap font-medium text-gray-900 dark:text-white">
+                                                    {albumInfo.map(song => {
+                                                        return (
+                                                            <p className="albumSong">{song?.track?.name}</p>
+                                                            )
+                                                    })}
+                                                </Table.Cell>
+                                            </Table.Row>
+                                                )
+                                        })}
                                 </Table.Body>
                             </Table>
                         </div>
@@ -98,7 +134,7 @@ function ArtistPage(props) {
                         <div>
                             <h2 className="artistHeading py-4">Bio</h2>
                             {
-                                bio && <div className="artistText py-3" dangerouslySetInnerHTML={createMarkup(bio)} />
+                                bio && <div className="artistText py-3 text-justify" dangerouslySetInnerHTML={createMarkup(bio)} />
                             }
                         </div>
                         <div>
