8691b81b6cfe02a5db3baee44a99d84ec0d052ca
chloemaivu <chloe.vu@live.co.uk>
2023-03-31 13:36:36 +0100

    CV: added styling

--- music-site_frontend/src/components/playlistCard.js
+++ music-site_frontend/src/components/playlistCard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Card } from "flowbite-react";
 import HeartOutline from "../resources/heart_outline.svg"
 import Heart from "../resources/heart_filled.svg"
+import LoadingSpinner from './spinner';
 
 function PlaylistCard(props) {
 
@@ -40,47 +41,49 @@ function PlaylistCard(props) {
             setVisibility("Public")
         }
     }, [playlist])
-
-    return (
-        <div className="min-w-full w-screen">
-            <Card key={playlist._id}>
-                <h5 className="mb-3 text-base text-center font-semibold text-gray-900 dark:text-white lg:text-xl">
-                    <span>{playlist.name}</span>
-                    <span className="ml-3 inline-flex items-center justify-center rounded bg-gray-200 px-2 py-0.5 text-xs font-medium text-gray-500 dark:bg-gray-700 dark:text-gray-400">
-                        {visibility}
-                    </span>
-                    <span>
-                        <img src={HeartOutline} />
-                    </span>
-                    {/* <span>
+  
+    return tracks?.length > 0 ? (
+            <div className="min-w-full w-screen">
+                <Card className="playlistDark" key={playlist._id}>
+                    <h5 className="playlistTitle flex flex-row mb-3 text-base text-center font-semibold text-gray-900 dark:text-white">
+                        <div className="text-center">
+                        <span className="text-white">{playlist.name}</span>
+                        <span className="ml-3 inline-flex items-center justify-center rounded bg-gray-200 px-2 py-0.5 text-xs font-medium text-gray-500 dark:bg-gray-700 dark:text-gray-400">
+                            {visibility}
+                        </span>
+                        </div>
+                        <span className="heartIcon justify-content-end">
+                            <img src={HeartOutline} width={35} />
+                        </span>
+                        {/* <span>
                         <img src={Heart} />
                     </span> */}
-                </h5>
-                <p className="text-sm text-center font-normal text-gray-500 dark:text-gray-400">
-                    {playlist.description}
-                </p>
-                <ul className="my-4 space-y-3">
-                    {tracks?.map((track, index) => {
-                        return (
-                            <>
-                                <li key={index}>
-                                    <div className="group flex items-center rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500">
-                                        <img className="resultsCard" src={track?.album?.images[0]?.url} style={{ width: "10%" }} alt={track?.name} />
-                                        <span className="ml-3 flex-1 whitespace-nowrap">
-                                            {track?.name}
-                                        </span>
-                                        <span className="ml-3 flex-1 justify-endwhitespace-nowrap">
-                                            {msConvert(track?.duration_ms)}
-                                        </span>
-                                    </div>
-                                </li>
-                            </>
-                        )
-                    })}
-                </ul>
-            </Card>
-        </div>
-    )
-}
+                    </h5>
+                    <p className="text-sm white-text text-center font-normal dark:text-gray-400">
+                        {playlist.description}
+                    </p>
+                    <ul className="my-4 space-y-3">
+                        {tracks?.map((track, index) => {
+                            return (
+                                <>
+                                    <li key={index}>
+                                        <div className="playlistBlack border group flex items-center rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500">
+                                            <img className="resultsCard" src={track?.album?.images[0]?.url} style={{ width: "10%" }} alt={track?.name} />
+                                            <span className="flex-grow-1 ml-3 flex-1 whitespace-nowrap">
+                                                {track?.name}
+                                            </span>
+                                            <span className="flex-grow-0 ml-3 flex-1 text-center whitespace-nowrap">
+                                                {msConvert(track?.duration_ms)}
+                                            </span>
+                                        </div>
+                                    </li>
+                                </>
+                            )
+                        })}
+                    </ul>
+                </Card>
+            </div>) :
+        (<LoadingSpinner/> )
+    }
 
-export default PlaylistCard;
+    export default PlaylistCard;
