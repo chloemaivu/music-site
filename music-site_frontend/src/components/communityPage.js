16ba9a2872a6e2f885467850ac406d5bc0801e2a
kbugaite <kbugaite@gmail.com>
2023-04-02 16:25:36 +0100

    communityPage updates

--- music-site_frontend/src/components/communityPage.js
+++ music-site_frontend/src/components/communityPage.js
@@ -1,14 +1,20 @@
 import { React, useState, useEffect } from 'react';
-import { Timeline, Card, Avatar, Badge } from "flowbite-react";
+import { Timeline, Avatar, Badge } from "flowbite-react";
 import { CalendarIcon } from "@heroicons/react/24/solid";
+import PlaylistCard from "./playlistCard";
 
 function CommunityPage(props) {
     const user = props.user
     const [playlists, setPlaylists] = useState({})
 
     useEffect(() => {
-      props.client.getPlaylists(window.localStorage.currentUserID).then((response) => setPlaylists(response))
-    }, [])
+        const prependUserID = "USERID::" + window.localStorage.currentUserID
+        props.client.getPlaylists(prependUserID).then((response) => {setPlaylists(response)})
+      }, [])
+
+    // useEffect(() => {
+    //   props.client.getPlaylists(window.localStorage.currentUserID).then((response) => setPlaylists(response))
+    // }, [])
 
     function Calendar() {
         return (
@@ -37,72 +43,35 @@ function CommunityPage(props) {
             {user.username}'s Timeline
         </h2>
     </div>
+
+    {/* Mapping through user's playlists here */}
+
     <div className="timelineContainer my-12">
     <Timeline className="userContainer pl-8">
+        <div className="timelinePlaylists">
+        {playlists ? playlists?.map((playlist) => {
+            if (!playlist.privacy)
+        return (
         <Timeline.Item>
-        <Timeline.Point icon={Calendar} />
+            <Timeline.Point icon={Calendar} />
             <Timeline.Content>
-            <Timeline.Time>
-                Hardcoded Date
+            <Timeline.Time className="text-5xl">
+            Date to be added
             </Timeline.Time>
-            <Timeline.Title>
-                Some hard coded title
+            <Timeline.Title className="text-3xl py-4">
+                {playlist.name}
             </Timeline.Title>
-            <Timeline.Body>
-                <Card href="#" className="playlist bg-gray-400 w-96">
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Playlist element goes here
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Tracks are rendered here
-                    </p>
-                </Card>
-            </Timeline.Body>
-            </Timeline.Content>
+                    <Timeline.Body>
+                        <PlaylistCard key={playlist._id} playlist={playlist} client={props.client} />
+                    </Timeline.Body>
+                </Timeline.Content>
         </Timeline.Item>
-        <Timeline.Item>
-        <Timeline.Point icon={Calendar} />
-            <Timeline.Content>
-            <Timeline.Time>
-                Previous Date
-            </Timeline.Time>
-            <Timeline.Title>
-                Some hard coded title
-            </Timeline.Title>
-            <Timeline.Body>
-                <Card href="#" className="playlist bg-gray-400 w-96">
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Another playlist element goes here
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Tracks are rendered here
-                    </p>
-                </Card>
-            </Timeline.Body>
-            </Timeline.Content>
-        </Timeline.Item>
-{/* MAP THROUGH TIMELINE ITEM HERE */}
-<Timeline.Item>
-        <Timeline.Point icon={Calendar} />
-            <Timeline.Content>
-            <Timeline.Time>
-                Previous Date
-            </Timeline.Time>
-            <Timeline.Title>
-                Some hard coded title
-            </Timeline.Title>
-            <Timeline.Body>
-                <Card href="#" className="playlist bg-gray-400 w-96">
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Another playlist element goes here
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Tracks are rendered here
-                    </p>
-                </Card>
-            </Timeline.Body>
-            </Timeline.Content>
-        </Timeline.Item>
+              )})
+            :
+            <>
+            </>
+            }
+        </div>
     </Timeline>
     </div>
     </>
