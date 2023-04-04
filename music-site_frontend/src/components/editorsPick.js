9100d98b765180bb6ba1dc0ad2f8a6ed1c60686e
chloemaivu <chloe.vu@live.co.uk>
2023-04-04 14:51:51 +0100

    CMV: Highlighted playlists displayed on homepage

--- music-site_frontend/src/components/editorsPick.js
+++ music-site_frontend/src/components/editorsPick.js
@@ -3,6 +3,7 @@ import { useEffect, useState, React } from "react";
 function EditorsPick(props) {
 
     const [playlists, setPlaylists] = useState({})
+    const [tracks, setTracks] = useState([])
 
     const getAllPlaylist = async () => {
         await props.client.getAllPlaylists().then((response) => filterPlaylists(response))
@@ -12,6 +13,17 @@ function EditorsPick(props) {
     const filterPlaylists = (playlists) => {
         const filteredPlaylists = playlists?.filter(playlist => playlist.highlighted === true)
         setPlaylists(filteredPlaylists)
+
+        const images = []
+        filteredPlaylists.map(playlist =>
+            images.push(playlist.uri[0].split(":")[2]))
+        getTracks(images)
+        console.log(images)
+    }
+
+    const getTracks = async (uri) => {
+        const data = await props.client.getTracks(uri)
+        setTracks(data.tracks)
     }
 
     useEffect(() => {
@@ -19,24 +31,30 @@ function EditorsPick(props) {
     }, [])
 
     console.log(playlists)
+    // tracks[i]?.album?.images[0]?.url
 
     return (
         <>
             <h2 className="text-5xl text-center">Editor's Picks</h2>
             {/* grid */}
             {playlists.length > 0 ? (<>
-
-                {
-                    playlists?.map((playlist, i) => {
-                        return (
-                            <div key={i}>
-                                <h2 className="text-5xl text-center">{playlist.name}</h2>
-                                <h2 className="text-5xl text-center">{playlist.description}</h2>
-                                <h2 className="text-5xl text-center">Image of first track of playlist</h2>
-                            </div>
-                        )
-                    })
-                }
+                <div className="grid grid-cols-5 gap-4" style={{ margin: "1%" }}>
+                    {
+                        playlists?.map((playlist, i) => {
+                            return (
+                                <div key={i} className="border">
+                                    <div>
+                                        <img src="" style={{ width: "100%" }} alt="first track image" />
+                                    </div>
+                                    <div className="text-center" >
+                                        <p className="text-3xl"><strong>{playlist.name}</strong></p>
+                                        <p className="text-xl">{playlist.description}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
 
             </>) : (<></>)}
 
